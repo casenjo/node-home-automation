4e7df21fba2094a80d8fcd8e34097d9cccf96df1
Cristian Asenjo <cristian@spoil.io>
2015-09-07 19:29:54 -0700

    Update test to use the node-sonytv module properly

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
 var assert = require("assert");
 
-var bravia = require('../modules/bravia');
+var bravia = require('../modules/node-sonytv').SonyTV('host', '/sony/IRCC', '/sony/accessControl', 'CLIENT_ID', 'NICKNAME');;
 
 var config = require('../config');
 
 describe('Sony Bravia TV', function() {
-  describe('#turnOn()', function () {
-    it('Should return true when the function is successfully called', function () {
-      assert.equal(true, bravia.turnOn(config.tv.mac));
+  describe('#powerOn()', function () {
+    it('Should return false when the function is called without a MAC address', function () {
+      assert.equal(false, bravia.powerOn());
     });
   });
 });
